c5cb23368083491acf7a1906e062c83da4f2a34e
weslley26 <Wesley.sjesus7@senacsp.com.br>
2025-03-27 17:15:22 -0300

    Folha de pagamentpo

---
+++ FolhaDePagamento/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace FolhaDePagamento
+{
+    partial class form1FolhaDePagamento
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFolhaDePagamento = new System.Windows.Forms.Label();
+            this.lblSalario = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ccbPlanoSaude = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lblClubeDeLazer = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblFolhaDePagamento
+            // 
+            this.lblFolhaDePagamento.AutoSize = true;
+            this.lblFolhaDePagamento.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFolhaDePagamento.Location = new System.Drawing.Point(45, 28);
+            this.lblFolhaDePagamento.Name = "lblFolhaDePagamento";
+            this.lblFolhaDePagamento.Size = new System.Drawing.Size(110, 20);
+            this.lblFolhaDePagamento.TabIndex = 0;
+            this.lblFolhaDePagamento.Text = "Data da Folha";
+            // 
+            // lblSalario
+            // 
+            this.lblSalario.AutoSize = true;
+            this.lblSalario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSalario.Location = new System.Drawing.Point(49, 148);
+            this.lblSalario.Name = "lblSalario";
+            this.lblSalario.Size = new System.Drawing.Size(58, 20);
+            this.lblSalario.TabIndex = 1;
+            this.lblSalario.Text = "Salário";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(480, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // ccbPlanoSaude
+            // 
+            this.ccbPlanoSaude.AutoSize = true;
+            this.ccbPlanoSaude.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ccbPlanoSaude.Location = new System.Drawing.Point(53, 216);
+            this.ccbPlanoSaude.Name = "ccbPlanoSaude";
+            this.ccbPlanoSaude.Size = new System.Drawing.Size(119, 24);
+            this.ccbPlanoSaude.TabIndex = 4;
+            this.ccbPlanoSaude.Text = "Plano Saúde";
+            this.ccbPlanoSaude.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(57, 341);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // lblClubeDeLazer
+            // 
+            this.lblClubeDeLazer.AutoSize = true;
+            this.lblClubeDeLazer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblClubeDeLazer.Location = new System.Drawing.Point(53, 298);
+            this.lblClubeDeLazer.Name = "lblClubeDeLazer";
+            this.lblClubeDeLazer.Size = new System.Drawing.Size(116, 20);
+            this.lblClubeDeLazer.TabIndex = 6;
+            this.lblClubeDeLazer.Text = "Clube de Lazer";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(480, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(483, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(486, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "label4";
+            // 
+            // form1FolhaDePagamento
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblClubeDeLazer);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.ccbPlanoSaude);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblSalario);
+            this.Controls.Add(this.lblFolhaDePagamento);
+            this.Name = "form1FolhaDePagamento";
+            this.Text = "Folha De Pagamento";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFolhaDePagamento;
+        private System.Windows.Forms.Label lblSalario;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox ccbPlanoSaude;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label lblClubeDeLazer;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
